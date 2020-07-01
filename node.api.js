be691802fef4dca2188ee6602a37c08dd0d006c1
Anton Strogonoff <anton@strogonoff.name>
2020-07-01 17:12:59 +0900

    feat: Try to make illustrations work in docs (WIP)

--- node.api.js
+++ node.api.js
@@ -4,25 +4,45 @@ import path from 'path';
 
 export default pluginOptions => ({
   afterExport: async state => {
-    const outPrefix = 'dist/_in_app_help/';
-    const prefix = 'desktop/docs/ui/'
+    const appDocsOutPrefix = 'dist/_in_app_help/';
+    const appDocsURLPrefix = 'desktop/docs/ui/'
+
+    const docsURLPrefix = 'desktop/docs/';
+    const docsSrcPrefix = 'docs/';
+    const docsOutPrefix = 'dist/desktop/docs'
 
     for (const r of state.routes) {
-      if (r.path.indexOf(prefix) === 0) {
-        const id = r.path.replace(prefix, '');
+
+      if (r.path.indexOf(docsURLPrefix) === 0) {
+        const id = r.path.replace(docsURLPrefix, '');
+        const _data = r.data?.docPage?.data;
+        if (!_data) {
+        } else {
+          const media = (_data.media || []);
+          for (const f of media) {
+            fs.copyFileSync(
+              `${docsSrcPrefix}/${path.dirname(id)}/${f}`,
+              `${docsOutPrefix}/${id}/${f}`);
+          }
+        }
+      }
+
+      if (r.path.indexOf(appDocsURLPrefix) === 0) {
+        const id = r.path.replace(appDocsURLPrefix, '');
         const _data = r.data?.docPage?.data;
         if (!_data || !id || !_data?.excerpt) {
-          continue;
+        } else {
+          const data = {
+            title: _data.title,
+            excerpt: _data.excerpt,
+            link: `/${r.path}`,
+          };
+          const dataJSON = JSON.stringify(data);
+          fs.mkdirSync(`${appDocsOutPrefix}${path.dirname(id)}`, { recursive: true });
+          fs.writeFileSync(`${appDocsOutPrefix}${id}.json`, dataJSON);
         }
-        const data = {
-          title: _data.title,
-          excerpt: _data.excerpt,
-          link: `/${r.path}`,
-        };
-        const dataJSON = JSON.stringify(data);
-        fs.mkdirSync(`${outPrefix}${path.dirname(id)}`, { recursive: true });
-        fs.writeFileSync(`${outPrefix}${id}.json`, dataJSON);
       }
+
     }
     return state
   },
