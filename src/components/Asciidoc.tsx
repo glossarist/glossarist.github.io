150635f80635ee367669cab05cd2d36cf455e65d
Anton Strogonoff <anton@strogonoff.name>
2020-07-01 20:19:29 +0900

    fix: Image paddings in Asciidoc

--- src/components/Asciidoc.tsx
+++ src/components/Asciidoc.tsx
@@ -31,11 +31,11 @@ const AsciidocStyled = styled.div`
     box-shadow: rgba(0, 0, 0, 0.15) .1rem .1rem 1rem;
   }
   .imageblock {
+    padding-top: .5rem;
+    padding-bottom: .5rem;
     .content {
       background: whiteSmoke;
       text-align: center;
-      padding-top: .5rem;
-      padding-bottom: .5rem;
     }
   }
 `
