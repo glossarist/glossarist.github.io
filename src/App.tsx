93d9d6ccebfdf8a9cf4fd328e3eb05cc32dc38ff
Anton Strogonoff <anton@strogonoff.name>
2020-07-01 14:51:38 +0900

    chore: Add general meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ const App: React.FC<{}> = function () {
 
       <Helmet>
         <title>Glossarist</title>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Helmet>
 
       <header>
