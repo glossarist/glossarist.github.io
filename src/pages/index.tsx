b49de9ec7e46a06b97f4763968471fd72db4ab0e
Anton Strogonoff <anton@strogonoff.name>
2020-07-06 13:40:17 +0900

    Update landing page, add users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Button } from 'components/Links'
+import { Button, Link } from 'components/Links'
 import { Lead } from 'components/typography'
 import { EntryPoints, EntryPoint, Audience } from '../components/entryPoints'
+import styled from 'styled-components'
+import isoSymbol from 'assets/iso-symbol.svg'
 
 
 const Page: React.FC<{}> = function () {
   return (
-    <div>
+    <>
       <Lead>
         <p>
           Open-source software suite
@@ -19,7 +21,7 @@ const Page: React.FC<{}> = function () {
           </EntryPoint>
           <EntryPoint>
             <Audience>Managing concept registries with&nbsp;Glossarist?</Audience>
-            <Button className="action" to="desktop">Learn desktop&nbsp;app</Button>
+            <Button className="action" to="desktop">Learn to&nbsp;use the desktop&nbsp;app</Button>
           </EntryPoint>
           <EntryPoint style={{ flex: 0.5 }}>
             <Audience>Operating infrastructure?</Audience>
@@ -27,8 +29,77 @@ const Page: React.FC<{}> = function () {
           </EntryPoint>
         </EntryPoints>
       </Lead>
-    </div>
+
+      <Section title="Used by" style={{ marginTop: '2rem', opacity: .5 }}>
+        <Users>
+          <li>
+            <img src={isoSymbol} />
+            <Link to="https://isotc211.geolexica.org/">
+              Geolexica for ISO/TC 211 MLGT
+            </Link>
+          </li>
+        </Users>
+      </Section>
+    </>
   )
 }
 
 export default Page
+
+
+const Section: React.FC<{
+  title: string
+  style?: React.CSSProperties
+  className?: string
+}> = function ({ title, style, className, children }) {
+  return (
+    <RowSection style={style} className={className}>
+      <h4 className="title">{title}</h4>
+      <div className="contents">{children}</div>
+    </RowSection>
+  )
+}
+
+const RowSection = styled.section`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-start;
+
+  & + & {
+    margin-top: 1rem;
+  }
+
+  .title {
+    text-transform: uppercase;
+    font-weight: normal;
+    margin: 1em 0;
+    padding: 0;
+    margin-right: 1rem;
+  }
+`
+
+
+const Users = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  > * {
+    display: block;
+    margin: 1em 0;
+    padding-left: 2.5em;
+    position: relative;
+
+    a:link, a:visited {
+      color: inherit;
+      text-decoration: none;
+    }
+    > img:first-child {
+      vertical-align: middle;
+      height: 2em;
+      top: -.35em;
+      left: 0;
+      position: absolute;
+    }
+  }
+`
