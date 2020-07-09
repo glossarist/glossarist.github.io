afe0c4d3fc7fda9e9e554495333e7af95d241cfe
Anton Strogonoff <anton@strogonoff.name>
2020-07-09 20:43:40 +0900

    fix: Avoid broken DL links on Desktop landing

--- src/containers/Desktop.tsx
+++ src/containers/Desktop.tsx
@@ -1,43 +1,86 @@
 import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
+
+import { Octokit } from '@octokit/rest'
+import { ReposGetLatestReleaseResponseData } from '@octokit/types'
+
 import { default as Page } from 'containers/Page'
 import { Button } from 'components/Links'
 import { Lead } from 'components/typography'
-import { Octokit } from '@octokit/rest'
 import { EntryPoints, EntryPoint } from 'components/entryPoints'
 
 
 const octokit = new Octokit()
 
+
 const repoOwner = 'glossarist'
 const repoName = 'glossarist-desktop'
 
 
+type OS = 'macOS' | 'Windows'
+
+
+function getGithubLink(
+  githubRepoOwner: string,
+  githubRepoName: string,
+): string {
+  return `https://github.com/${githubRepoOwner}/${githubRepoName}`
+}
+
+
 export default () => {
-  const [releaseName, setReleaseName] = useState<string | undefined>(undefined)
+  const [releaseData, setReleaseData] =
+    useState<ReposGetLatestReleaseResponseData | undefined>(undefined);
+
+  const [userOS, setUserOS] =
+    useState<OS | undefined>(undefined)
+
+  const [specificDLLink, setSpecificDLLink] =
+    useState<string | undefined>(undefined);
+
+  const releaseName = releaseData?.name
 
   useEffect(() => {
     (async () => {
       const release = await octokit.repos.getLatestRelease({
-        owner: 'glossarist',
-        repo: 'glossarist-desktop',
+        owner: repoOwner,
+        repo: repoName,
       })
-
-      setReleaseName(release.data.name)
+      setReleaseData(release.data);
     })()
   }, [])
 
-  let dlMac: string | undefined
-  let dlWinPort: string | undefined
-  const dlDefault = `https://github.com/${repoOwner}/${repoName}/releases/`
+  useEffect(() => {
+    if (userOS && releaseName) {
+      const assets = releaseData.assets
 
-  if (releaseName) {
-    dlMac = `https://github.com/${repoOwner}/${repoName}/releases/download/v${releaseName}/glossarist-desktop-${releaseName}.dmg`
-    dlWinPort = `https://github.com/${repoOwner}/${repoName}/releases/download/v${releaseName}/glossarist-desktop-${releaseName}-portable.exe`
-  } else {
-    dlMac = undefined
-    dlWinPort = undefined
-  }
+      let expectedOSAssetName: string
+      if (userOS === 'Windows') {
+        expectedOSAssetName = `glossarist-desktop-${releaseName}-portable.exe`
+      } else if (userOS === 'macOS') {
+        expectedOSAssetName = `glossarist-desktop-${releaseName}.dmg`
+      }
+
+      const asset = assets.find(a => a.name === expectedOSAssetName)
+
+      if (asset && asset.browser_download_url) {
+        setSpecificDLLink(asset.browser_download_url);
+      }
+    }
+  }, [userOS, releaseData])
+
+  useEffect(() => {
+    if (window?.navigator) {
+      const ua = window.navigator.userAgent
+      if (ua.indexOf('Mac') >= 0) {
+        setUserOS('macOS')
+      } else if (ua.indexOf('Windows')) {
+        setUserOS('Windows')
+      }
+    }
+  }, [])
+
+  const releasesURL = `${getGithubLink(repoOwner, repoName)}/releases`;
 
   return (
     <Page title="Glossarist Desktop">
@@ -50,31 +93,23 @@ export default () => {
         <p>
           Manage a concept system from an app that runs on your computer.
         </p>
-        <br />
-        <Button to="docs/getting-started">Get started</Button>
+
+        <EntryPoints>
+          <EntryPoint>
+            <Button to="docs/getting-started">Get started</Button>
+          </EntryPoint>
+          <EntryPoint>
+
+            {userOS && specificDLLink
+              ? <Button to={specificDLLink}>
+                  Download{releaseName ? ` v${releaseName}` : null} for {userOS}
+                </Button>
+              : <Button to={releasesURL}>
+                  View releases
+                </Button>}
+          </EntryPoint>
+        </EntryPoints>
       </Lead>
-
-      <br />
-
-      <h3>Download{releaseName ? ` latest release (v${releaseName})` : null}</h3>
-
-      <EntryPoints>
-        {!dlMac || !dlWinPort
-          ? <EntryPoint>
-              <Button to={dlDefault}>Latest release</Button>
-            </EntryPoint>
-          : null}
-        {dlMac
-          ? <EntryPoint>
-              <Button to={dlMac}>macOS</Button>
-            </EntryPoint>
-          : null}
-        {dlWinPort
-          ? <EntryPoint>
-              <Button to={dlWinPort}>Windows</Button>
-            </EntryPoint>
-          : null}
-      </EntryPoints>
 
     </Page>
   )
