2012bfecc74b29f0f937676fb27ee06e9eae99c2
Anton Strogonoff <anton@strogonoff.name>
2020-08-17 15:15:52 +0900

    fix: correct missing whitespace below Ribose logo

--- src/components/MaintainingOrgBanner.tsx
+++ src/components/MaintainingOrgBanner.tsx
@@ -7,7 +7,11 @@ import organizationLogoImage from '../assets/riboseopen-logo-dark.svg'
 const MaintainingOrgBanner: React.FC<{}> = function () {
   return (
     <UnstyledLink to="https://open.ribose.com">
-      <SymbolImage size={10} src={organizationLogoImage} className="org-logo" />
+      <SymbolImage
+        style={{ marginBottom: '1rem' }}
+        size={10}
+        src={organizationLogoImage}
+        className="org-logo" />
     </UnstyledLink>
   )
 }
